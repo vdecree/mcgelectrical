398d1190ea92d495268bd2154421ea8635735a7e
vdecree <hello@matdoidge.co.uk>
2023-03-23 13:25:26 +0000

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,40 @@
+// Import plugins
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+
+// Import filters
+const cssMinFilter = require('./src/filters/css-min.js');
+
+module.exports = (eleventyConfig) => {
+  // Add plugins
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(pluginRss);
+
+  // Add filters
+  eleventyConfig.addFilter('cssmin', cssMinFilter);
+
+  // Add tailwind
+  eleventyConfig.addTemplateFormats("tailwind");
+
+  // Pass through
+  eleventyConfig.addPassthroughCopy('./src/img/');
+  eleventyConfig.addPassthroughCopy('./src/fonts/');
+  eleventyConfig.addPassthroughCopy('./src/js/');
+  eleventyConfig.addPassthroughCopy('./src/android-chrome-192x192.png');
+  eleventyConfig.addPassthroughCopy('./src/android-chrome-384x384.png');
+  eleventyConfig.addPassthroughCopy('./src/apple-touch-icon.png');
+  eleventyConfig.addPassthroughCopy('./src/favicon-16x16.png');
+  eleventyConfig.addPassthroughCopy('./src/favicon-32x32.png');
+  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('./src/safari-pinned-tab.svg');
+  eleventyConfig.addPassthroughCopy('./src/site.webmanifest');
+  eleventyConfig.addPassthroughCopy('./src/robots.txt');
+
+  return {
+    dir: {
+      input: 'src',
+      output: '_site',
+    },
+    passthroughFileCopy: true,
+  };
+};
